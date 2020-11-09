5d284940afe1227fb8195fd37a5dffa9b7d40eee
seungeun <seungeun.yeon71@gmail.com>
2020-11-09 10:06:36 +0900

    add: context api practive initial commit

---
+++ src/contexts/color.js
@@ -0,0 +1,31 @@
+import { createContext, useState } from 'react';
+
+const ColorContext = createContext({
+  state: { color: 'black', subcolor: 'red' },
+  actions: {
+    setColor: () => {},
+    setSubColor: () => {},
+  },
+});
+
+const ColorProvider = ({ children }) => {
+  const [color, setColor] = useState('black');
+  const [subcolor, setSubcolor] = useState('red');
+
+  const value = {
+    state: { color, subcolor },
+    actions: { setColor, setSubcolor },
+  };
+
+  return (
+    <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+  );
+};
+
+// const ColorConsumer = ColorContext.Consumer 와 같은 의미
+const { Consumer: ColorConsumer } = ColorContext;
+
+// ColorProvider 와 ColorConsumer 내보내기
+export { ColorProvider, ColorConsumer };
+
+export default ColorContext;
